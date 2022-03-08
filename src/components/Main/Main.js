5a3e5583641f5f1e15cc5a5a73795a3730106bdc
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-07 16:42:51 -0800

    background image rendering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Animal from '../Animal/Animal';
 import './Main.css';
 import { animals } from '../../data';
+import background from '../../background.png';
 
 export default function Main() {
   return (
-    <div>
+    <main style={{ backgroundImage: `url(${background})` }}>
       {animals.map((animal) => (
         <Animal key={animal.name} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
       ))}
 
-    </div>
+    </main>
   );
 }
